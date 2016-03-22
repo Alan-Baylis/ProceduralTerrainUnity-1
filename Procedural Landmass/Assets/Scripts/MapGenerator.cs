2824b8de5ecf3ef8faa6479b7c4beb792f05147d
Cameron Fraser <camfrasertech@gmail.com>
2016-03-21 22:57:50 -0700

    Assigned colors to heights

--- Procedural Landmass/Assets/Scripts/MapGenerator.cs
+++ Procedural Landmass/Assets/Scripts/MapGenerator.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class MapGenerator : MonoBehaviour {
 
+	public enum DrawMode {NoiseMap, ColorMap};
+	public DrawMode drawMode;
+
 	public int mapWidth;
 	public int mapHeight;
 	public float noiseScale;
@@ -15,13 +18,33 @@ public class MapGenerator : MonoBehaviour {
 
 	public bool autoUpdate;
 
+	public TerrainType[] regions;
+
 	public void GenerateMap ()
 	{
-		float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
+		float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
 
-		MapDisplay display = FindObjectOfType<MapDisplay>();
+		Color[] colorMap = new Color[mapWidth * mapHeight];
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				float currentHeight = noiseMap [x, y];
+				for (int i = 0; i < regions.Length; i++) {
+					if (currentHeight <= regions [i].height) {
+						colorMap [y * mapWidth + x] = regions [i].color;
+						break;
+					}
+				}
+			}
+		}
 
-		display.DrawNoiseMap(noiseMap);
+		MapDisplay display = FindObjectOfType<MapDisplay> ();
+
+		if (drawMode == DrawMode.NoiseMap) {
+			display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+		} else if (drawMode == DrawMode.ColorMap) {
+			display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+		}
+
 	}
 
 	void OnValidate ()
@@ -42,4 +65,11 @@ public class MapGenerator : MonoBehaviour {
 			octaves = 0;
 		}
 	}
+}
+
+[System.Serializable]
+public struct TerrainType {
+	public string name;
+	public float height;
+	public Color color;
 }
